8c8f7be9b375ea784365835c761bbada794c7700
yeongmin369 <143806113+yeongmin369@users.noreply.github.com>
2023-09-20 15:44:32 +0900

    Add files via upload

--- lecture/python_02_string.py
+++ lecture/python_02_string.py
@@ -38,3 +38,56 @@ print(msg[3:]) # 끝 인덱스 생략 -> 자동 -1 입력
 print(msg[18:24])
 # 역방향 인덱스 ->
 print(msg[-4:-1])
+
+# 4. 문자열 함수
+str = "Hello World"
+print("="*100)
+# 4-1. len() : 문자열 길이 계산
+print(len(str))
+
+# 4-2. upper() and lower() : 대소문자 변경
+# - 데이터 전처리: 1A, 1a => 1A 통일(upper())
+print(str.upper())
+print(str.lower())
+
+# 4-3. replace() : 문자열 내의 특정 문자 치환
+print(str.replace("H","J"))
+
+# 4-4. split() : 구분자를 기준으로 문자열 분할(Default: 공백)
+b = "hello world what a nice weather"
+print(b.split("w"))
+print(b.split())
+
+# 4-5.strip() : 문자열의 좌우 공백을 제거
+id = ""
+print(id)
+print(id.strip())
+
+# 4-6. find() and rfind() : 문자열 내부의 특정 문자 위치 인덱스 출력
+print(str.find("o")) # Hell*o* world
+print(str.rfind("o")) # world w*o*rld
+print(str.find("world")) # 없으면 -1 출력
+print(str.find("World")) # 첫글자의 인덱스 출력
+print(str.rfind("World")) # 첫글자의 인덱스 출력
+
+# 4-7. in() : 특정 문자열 포함하는지 확인 (True, False 출력)
+print("Hi" in "Hi python")
+
+# -> id만 추출 "cherry1004"
+#    "job1234@gmail.com"
+#    "abc@gmail.com"
+
+id = "cherry1004@gmail.com"
+val = id[:id.find("@")]
+print(val) # "cherry1004" 출력
+
+# www.naver.com
+# www.daum.net
+# www.google.com
+
+# naver, daum, google만 추출 코드
+url = "www.naver.com"
+start = url.find(".") + 1
+end = url.rfind(".")
+val = url[start:end]
+print(val) # naver
